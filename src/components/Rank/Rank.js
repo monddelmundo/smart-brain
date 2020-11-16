8d945a5194d2d38515823a01acdbd033ef4a9ea4
Edmond Del Mundo <edmond.delmundo@gmail.com>
2020-11-16 17:49:18 +0800

    Added fix to bugs

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState("");
+
+  useEffect(() => {
+    generateEmoji(entries);
+  }, [entries]);
+
+  const generateEmoji = (entries) => {
+    fetch(
+      `https://3cl05dqgbg.execute-api.ap-southeast-1.amazonaws.com/prod/rank?rank=${entries}`
+    )
+      .then((response) => response.json())
+      .then((data) => setEmoji(data.input))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
-      <div className='white f3'>
+      <div className="white f3">
         {`${name}, your current entry count is...`}
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
+      <div className="white f1">{entries}</div>
+      <div className="white f3">{`Rank Badge: ${emoji}`}</div>
     </div>
   );
-}
+};
 
 export default Rank;
